4999e3bd73ac30c3f5561d20f714a88bd74908a2
Dio <a0981551262@gmail.com>
2023-03-24 01:16:28 +0800

    debug of delta_degree_cal has been done to do list: debug scanf function provide the variable of calculation trace-tracking function

--- main.py
+++ main.py
@@ -35,28 +35,27 @@ robot_arm2 = vp.cylinder(pos=vp.vec(0, robot_arm1_length+floor_depth/2, 0), radi
 
 def theta_calculation(x, y, n):
     if a != 0 and b != 0:
-        cos_theta = x / math.sqrt(a ** 2 + b ** 2)
-        sin_theta = y / math.sqrt(a ** 2 + b ** 2)
+        cos_theta = x / math.sqrt(x ** 2 + y ** 2)
+        sin_theta = y / math.sqrt(x ** 2 + y ** 2)
         if n == 1:
             return cos_theta
         elif n == 0:
             return sin_theta
 
 
-def psi_calculation(x, y, z, n):
-    if robot_arm2_length != 0 and robot_arm2_length != 0:
-        cos_psi = (robot_arm2_length**2+robot_arm1_length**2-(x**2+y**2+z**2))/(2*robot_arm2_length*robot_arm1_length)
-        sin_psi = math.sqrt(1-cos_psi**2)
-        if n == 1:
-            return cos_psi
-        elif n == 0:
-            return sin_psi
+def psi_calculation(x, z, y, n):
+    cos_psi = (robot_arm2_length**2+robot_arm1_length**2-(x**2+y**2+z**2))/(2*robot_arm2_length*robot_arm1_length)
+    sin_psi = math.sqrt(1-cos_psi**2)
+    if n == 1:
+        return cos_psi
+    elif n == 0:
+        return sin_psi
 
 
-def phi_calculation(x, y, z, n):
-    sin_phi0 = c/math.sqrt(x**2+y**2+z**2)
+def phi_calculation(x, z, y, n):
+    sin_phi0 = z/math.sqrt(x**2+y**2+z**2)
     cos_phi0 = math.sqrt(1-sin_phi0**2)
-    sin_phi1 = robot_arm2_length*psi_calculation(x, y, z, 0)/math.sqrt(x**2+y**2+z**2)
+    sin_phi1 = robot_arm2_length*psi_calculation(x, z, y, 0)/math.sqrt(x**2+y**2+z**2)
     cos_phi1 = math.sqrt(1-sin_phi1**2)
     cos_phi = cos_phi1*cos_phi0-sin_phi1*sin_phi0
     sin_phi = sin_phi1*cos_phi0+sin_phi0*cos_phi1
@@ -66,24 +65,18 @@ def phi_calculation(x, y, z, n):
         return sin_phi
 
 
-def delta_theta_cal(x, y, x_n, y_n):
-    sin_delta_theta = theta_calculation(x_n, y_n, 0)*theta_calculation(x, y, 1)-theta_calculation(x_n, y_n, 1)*theta_calculation(x, y, 0)
-    result = math.asin(sin_delta_theta)
-    return result
+def delta_degree_cal(x, z, y, x_n, z_n, y_n, n):
+    if n == 1:
+        sin_delta_degree = theta_calculation(x_n, y_n, 0)*theta_calculation(x, y, 1) - \
+                           theta_calculation(x_n, y_n, 1)*theta_calculation(x, y, 0)
+    elif n == 2:
+        sin_delta_degree = phi_calculation(x_n, z_n, y_n, 0) * phi_calculation(x, z, y, 1) - \
+                           phi_calculation(x_n, z_n, y_n, 1) * phi_calculation(x, z, y, 0)
+    elif n == 3:
+        sin_delta_degree = psi_calculation(x_n, z_n, y_n, 0) * psi_calculation(x, z, y, 1) - \
+                           psi_calculation(x_n, z_n, y_n, 1) * psi_calculation(x, z, y, 0)
+    return math.asin(sin_delta_degree)
 
-
-def delta_phi_cal(x, y, z, x_n, y_n, z_n):
-    sin_delta_phi = phi_calculation(x_n, y_n, z_n, 0) * phi_calculation(x, y, z, 1) - \
-                    phi_calculation(x_n, y_n, z_n, 1) * phi_calculation(x, y, z, 0)
-    result = math.asin(sin_delta_phi)
-    return result
-
-
-def delta_psi_cal(x, y, z, x_n, y_n, z_n):
-    sin_delta_psi = psi_calculation(x_n, y_n, z_n, 0) * psi_calculation(x, y, z, 1) - \
-                    psi_calculation(x_n, y_n, z_n, 1) * psi_calculation(x, y, z, 0)
-    result = math.asin(sin_delta_psi)
-    return result
 
 temp_vec = vp.vec(0,0,0)
 def rotate_2d(degree):
@@ -109,10 +102,11 @@ def rotate_3d(degree, x_0, y_0, z_0):
                                 z_0*(math.cos(degree)+(1-math.cos(degree))*unit_revolve_vec.z**2)
 
 
-delta_theta = delta_theta_cal(a, b, a_n, b_n)
-delta_phi = delta_phi_cal(a, b, c, a_n, b_n, c_n)
-delta_psi = delta_psi_cal(a, b, c, a_n, b_n, c_n)
+print(delta_degree_cal(a, c, b, a_n, c_n, b_n, 1))
+print(delta_degree_cal(a, c, b, a_n, c_n, b_n, 2))
+print(delta_degree_cal(a, c, b, a_n, c_n, b_n, 3))
 t = 0
+'''
 while delta_theta != 0 or delta_phi != 0 or delta_psi != 0:
     vp.rate(1000)
     if delta_theta != 0 and delta_theta>0:
@@ -144,4 +138,4 @@ while delta_theta != 0 or delta_phi != 0 or delta_psi != 0:
     joint02.pos = current_robot_arm1_axis + vp.vec(0, floor_depth/2, 0)
     joint02.pos = joint02.pos + current_robot_arm2_axis
 
-
+'''
