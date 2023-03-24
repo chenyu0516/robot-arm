15957441e47a021444d8dab6bc8af4241af0ad58
Dio <a0981551262@gmail.com>
2023-03-24 15:20:00 +0800

    debug of delta_degree_cal has been done to do list: the delta_psi need to be <1 the matrix calculation looks great fix the remaining problem scanf function provide the variable of calculation trace-tracking function

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
 import vpython as vp
-import math
+import numpy as math
 
 
 # parameter
 floor_depth = 0.1
 robot_arm1_length = 2
 robot_arm2_length = 2
-original_point = vp.vec(0, floor_depth/2, 0)
+original_point = vp.vec(0, 0, 0)
+#initial coordinate
 a = robot_arm2_length
-b = robot_arm1_length+floor_depth/2
+b = robot_arm1_length
 c = 0
-a_n = 0.3
-b_n = 0.4
-c_n = 0.5
+a_n = 1
+b_n = 1.6
+c_n = 2
 current_robot_arm1_axis = vp.vec(0, robot_arm1_length, 0)
 current_robot_arm2_axis = vp.vec(robot_arm2_length, 0, 0)
 rotate_speed = 0.01
 
 scene = vp.canvas(title='robot arm', width=800, height=600, x=0, y=0, center=vp.vec(0, 0.1, 0),
                   background=vp.vec(0, 0.6, 0.6))
-floor = vp.box(pos=vp.vec(0, 0, 0), size=vp.vec(5, floor_depth, 5), color=vp.color.black)
+floor = vp.box(pos=vp.vec(0, floor_depth/2, 0), size=vp.vec(5, floor_depth, 5), color=vp.color.black)
 joint01 = vp.sphere(pos=original_point, radius=0.2, color=vp.color.blue)
 #joint type is sphere
-joint02 = vp.sphere(pos=vp.vec(0, robot_arm1_length+floor_depth/2, 0), radius=0.2, color=vp.color.blue)
+joint02 = vp.sphere(pos=vp.vec(0, robot_arm1_length, 0), radius=0.2, color=vp.color.blue)
 #joint type is ratation
 joint03 = vp.sphere(pos=vp.vec(a, b, c), radius=0.16, color=vp.color.blue)
 
 robot_arm1 = vp.cylinder(pos=original_point, radius=0.08, axis=vp.vec(0, 1, 0), length=robot_arm1_length,
                          color=vp.color.cyan)
-robot_arm2 = vp.cylinder(pos=vp.vec(0, robot_arm1_length+floor_depth/2, 0), radius=0.08, axis=vp.vec(1, 0, 0),
+robot_arm2 = vp.cylinder(pos=vp.vec(0, robot_arm1_length, 0), radius=0.08, axis=vp.vec(1, 0, 0),
                          length=robot_arm2_length, color=vp.color.cyan)
 
 
@@ -75,38 +76,62 @@ def delta_degree_cal(x, z, y, x_n, z_n, y_n, n):
     elif n == 3:
         sin_delta_degree = psi_calculation(x_n, z_n, y_n, 0) * psi_calculation(x, z, y, 1) - \
                            psi_calculation(x_n, z_n, y_n, 1) * psi_calculation(x, z, y, 0)
-    return math.asin(sin_delta_degree)
+    else:
+        sin_delta_degree = 0
+    return math.arcsin(sin_delta_degree)
 
 
-temp_vec = vp.vec(0,0,0)
-def rotate_2d(degree):
-    x_0 = current_robot_arm1_axis.x
-    y_0 = current_robot_arm1_axis.y
-    z_0 = current_robot_arm1_axis.z
-    current_robot_arm1_axis.x = x_0*math.cos(degree)-y_0*math.sin(degree)
-    current_robot_arm1_axis.y = x_0*math.sin(degree)+y_0*math.cos(degree)
-    current_robot_arm1_axis.z = z_0
+def rotate_matrix(x, z, y, theta1, theta2, n):
+    global current_robot_arm1_axis
+    matrix_a = math.array([
+                    [math.cos(theta1), -math.sin(theta1), 0],
+                    [math.sin(theta1), math.cos(theta1), 0],
+                    [0, 0, 1]
+    ])
+    axis1 = math.array([x, y, 0])
+    axis2 = math.array([0, 0, 1], dtype=float)
+    if x == 0 and y == 0:
+        rotate_axis = math.array([0, 1, 0])
+        mag_rotate_axis = 1
+    else:
+        rotate_axis = math.cross(axis1, axis2)
+        mag_rotate_axis = math.sqrt(rotate_axis.item(0)**2+rotate_axis.item(1)**2+1)
+    e_x = rotate_axis.item(0)/mag_rotate_axis
+    e_y = rotate_axis.item(1)/mag_rotate_axis
+    matrix_b = math.array([
+                           [(math.cos(theta2)+(1-math.cos(theta2)))*e_x**2, (1-math.cos(theta2))*e_x*e_y, math.sin(theta2)*e_y],
+                           [(1-math.cos(theta2))*e_x*e_y, (math.cos(theta2)+(1-math.cos(theta2)))*e_y**2, -math.sin(theta2)*e_x],
+                           [-math.sin(theta2)*e_y, math.sin(theta2)*e_x, 0]
+    ])
+    axis3_matrix = math.array([x, y, z])
+    temp = math.dot(matrix_b, axis3_matrix)
+    result = math.dot(matrix_a, temp)
+    if n == 1:
+        current_robot_arm1_axis.x = result.item(0)
+        current_robot_arm1_axis.y = result.item(1)
+        current_robot_arm1_axis.z = result.item(2)
+    elif n == 2:
+        current_robot_arm2_axis.x = result.item(0)
+        current_robot_arm2_axis.y = result.item(1)
+        current_robot_arm2_axis.z = result.item(2)
 
-def rotate_3d(degree, x_0, y_0, z_0):
-    vec_a = vp.vec(x_0, 0, y_0)
-    vec_b = vp.vec(0, z_0, 0)
-    unit_revolve_vec = vp.cross(vec_a, vec_b)/(vec_b.mag*vec_a.mag)
-    temp_vec.x = x_0*(math.cos(degree)+(1-math.cos(degree))*unit_revolve_vec.x**2)+\
-                                y_0*((1-math.cos(degree))*unit_revolve_vec.x*unit_revolve_vec.y-math.sin(degree)*unit_revolve_vec.z)+\
-                                z_0*((1-math.cos(degree))*unit_revolve_vec.x*unit_revolve_vec.y+math.sin(degree)*unit_revolve_vec.y)
-    temp_vec.y = x_0*((1-math.cos(degree))*unit_revolve_vec.x*unit_revolve_vec.y+math.sin(degree)*unit_revolve_vec.z)+\
-                                y_0*(math.cos(degree)+(1-math.cos(degree))*unit_revolve_vec.y**2)+\
-                                z_0*((1-math.cos(degree))*unit_revolve_vec.z*unit_revolve_vec.y+math.sin(degree)*unit_revolve_vec.x)
-    temp_vec.z = x_0*((1-math.cos(degree))*unit_revolve_vec.x*unit_revolve_vec.z-math.sin(degree)*unit_revolve_vec.y)+\
-                                y_0*((1-math.cos(degree))*unit_revolve_vec.y*unit_revolve_vec.z+math.sin(degree)*unit_revolve_vec.x)+\
-                                z_0*(math.cos(degree)+(1-math.cos(degree))*unit_revolve_vec.z**2)
+
+delta_theta = delta_degree_cal(a, c, b, a_n, c_n, b_n, 1)
+delta_phi = delta_degree_cal(a, c, b, a_n, c_n, b_n, 2)
+delta_psi = delta_degree_cal(a, c, b, a_n, c_n, b_n, 3)
+rotate_matrix(current_robot_arm1_axis.x, current_robot_arm1_axis.z, current_robot_arm1_axis.y, delta_theta, delta_phi, 1)
+rotate_matrix(current_robot_arm2_axis.x, current_robot_arm2_axis.z, current_robot_arm2_axis.y, delta_theta, delta_psi, 2)
+
+robot_arm1.axis = current_robot_arm1_axis
+joint02.pos = current_robot_arm1_axis
+joint03.pos = vp.vec(a_n, c_n, b_n)
+robot_arm2.pos = current_robot_arm1_axis
+robot_arm2.axis = current_robot_arm2_axis
 
 
-print(delta_degree_cal(a, c, b, a_n, c_n, b_n, 1))
-print(delta_degree_cal(a, c, b, a_n, c_n, b_n, 2))
-print(delta_degree_cal(a, c, b, a_n, c_n, b_n, 3))
-t = 0
+
 '''
+
 while delta_theta != 0 or delta_phi != 0 or delta_psi != 0:
     vp.rate(1000)
     if delta_theta != 0 and delta_theta>0:
